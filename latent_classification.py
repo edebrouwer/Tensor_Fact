be4d0e3bb79ae8e5ef77aab56e095dcd70961e68
Edward De Brouwer <edwarddebrouwer@Edwards-Machine.local>
2018-05-29 13:23:45 +0200

    better class

--- latent_classification.py
+++ latent_classification.py
@@ -4,7 +4,7 @@ import pandas as pd
 from sklearn import svm
 from tensor_fact_test import load_current_model
 
-mod=load_current_model()
-latent_pat=mod.pat_lat.weight
+file_path="./trained_models/8_dim_500epochs_lr02"
+latent_pat=torch.load(file_path+"current_model.pt")["pat_lat.weight"]
 tags=pd.read_csv("~/Data/MIMIC/death_tag_tensor.csv").sort_values("UNIQUE_ID")
 tag_mat=tags[["DEATHTAG","UNIQUE_ID"]].as_matrix()
