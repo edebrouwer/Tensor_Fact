d523ed181afe1f0e132b45df5f04ce765319ec48
edebrouwer <edward.debrouwer@gmail.com>
2018-06-15 12:03:26 +0200

    macau computations'

--- extract_macau_latents.py
+++ extract_macau_latents.py
@@ -1,20 +1,23 @@
 import numpy as np
+import progressbar
 
 #loading_latent_matrices
-dir_path="results_macau_16/"
-file_path="16_macau_"
+dir_path="results_macau_25/"
+file_path="25_macau-"
 N=400
 
 mean_lat_pat=0
 mean_lat_meas=0
 mean_lat_time=0
-for n in range(N):
+for n in progressbar.progressbar(range(1,N+1)):
     mean_lat_pat+=np.loadtxt(dir_path+file_path+"sample%d-U1-latents.csv"%n,delimiter=",")
-    mean_lat_meas+=np.loadtxt(dir_path+file_path+"sample%d-U2-latents.csv"%n,delimiter=",")
-    mean_lat_time+=np.loadtxt(dir_path+file_path+"sample%d-U3-latents.csv"%n,delimiter=",")
+    #mean_lat_meas+=np.loadtxt(dir_path+file_path+"sample%d-U2-latents.csv"%n,delimiter=",")
+    #mean_lat_time+=np.loadtxt(dir_path+file_path+"sample%d-U3-latents.csv"%n,delimiter=",")
 
 mean_lat_pat/=N
-mean_lat_meas/=N
-mean_lat_time/=N
+np.save(dir_path+"mean_pat_latent.npy",mean_lat_pat)
 
-print(mean_lat_meas)
+print("Loaded")
+
+
+
--- macau_mimic.py
+++ macau_mimic.py
@@ -1,7 +1,8 @@
 
 
 import macau
-import pandas as import pd
+import pandas as pd
+import numpy as np
 
 dir_path="~/Data/MIMIC/"
 lab_short=pd.read_csv(dir_path+"lab_short_tensor_train.csv")
@@ -11,6 +12,13 @@ lab_short_val=pd.read_csv(dir_path+"lab_short_tensor_val.csv")
 df_val=lab_short[["UNIQUE_ID","LABEL_CODE","TIME_STAMP","VALUENORM"]]
 
 cov_values=[chr(i) for i in range(ord('A'),ord('A')+18)]
-covariates=lab_short.groupby("UNIQUE_ID").first()[cov_values].as_matrix()
+cov_u=lab_short.groupby("UNIQUE_ID").first()[cov_values].as_matrix()
+cov_t=np.expand_dims(np.arange(101),axis=1)
 
-results=macau.macau(Y=df,Ytest=df_train,side=[covariates,None,None],num_latents=8,verbose=True,burnin=800)
+print(cov_u.shape)
+print(cov_t.shape)
+
+results=macau.macau(Y=df,Ytest=df_val,side=[cov_u,None,cov_t],num_latent=25,verbose=True,burnin=400,precision="adaptive",save_prefix="25_macau")
+
+
+
